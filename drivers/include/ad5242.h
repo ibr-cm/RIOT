76f42a44588f3d84a2934320b403b22e115c16b4
Rasmus Antons <mail@rasmusantons.de>
2017-12-01 12:22:51 +0100

    Added driver for ad5242

---
+++ drivers/include/ad5242.h
@@ -0,0 +1,36 @@
+#ifndef AD5242_H
+#define AD5242_H
+
+#include <stdint.h>
+#include "periph/i2c.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define AD5242_CHN_A				0
+#define AD5242_CHN_B				(1 << 7)
+#define AD5242_MID_RESET_DISABLE	0
+#define AD5242_MID_RESET_ENABLE		(1 << 6)
+#define AD5242_SHDN_OFF				0
+#define AD5242_SHDN_ON				(1 << 5)
+#define AD5242_OUT_1_LOW			0
+#define AD5242_OUT_1_HIGH			(1 << 4)
+#define AD5242_OUT_2_LOW			0
+#define AD5242_OUT_2_HIGH			(1 << 3)
+
+typedef struct {
+	i2c_t i2c;
+	uint8_t addr;
+	uint8_t opt;
+} ad5242_t;
+
+int ad5242_init(ad5242_t *dev, i2c_t i2c, i2c_speed_t speed, uint8_t addr, uint8_t opt);
+uint8_t ad5242_get_reg(ad5242_t *dev);
+void ad5242_set_reg(ad5242_t *dev, uint8_t data);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* AD5242_H */
