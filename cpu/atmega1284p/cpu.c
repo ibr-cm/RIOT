3f4623ee4c46fa21882f24ee5af9fc7b415ac8a3
Robert Hartung <hartung@ibr.cs.tu-bs.de>
2017-12-05 15:21:26 +0100

    cpu/atmega1284p: power down peripherals by default (merge confilcts resolved)

--- cpu/atmega1284p/cpu.c
+++ cpu/atmega1284p/cpu.c
@@ -21,12 +21,45 @@
 
 #include "cpu.h"
 #include "periph/init.h"
+#include <avr/wdt.h>
+
+#if (__AVR_LIBC_VERSION__ >= 10700UL)
+/* The proper way to set the signature is */
+#include <avr/signature.h>
+#else
+
+/* signature API not available before avr-lib-1.7.0. Do it manually.*/
+typedef struct {
+    const unsigned char B2;
+    const unsigned char B1;
+    const unsigned char B0;
+} __signature_t;
+#define SIGNATURE __signature_t __signature __attribute__((section(".signature")))
+SIGNATURE = {
+    .B2 = 0x05, //SIGNATURE_2, //ATMEGA1284p
+    .B1 = 0x97, //SIGNATURE_1, //128KB flash
+    .B0 = 0x1E, //SIGNATURE_0, //Atmel
+};
+#endif /* (__AVR_LIBC_VERSION__ >= 10700UL) */
 
 /**
  * @brief Initialize the CPU, set IRQ priorities
  */
 void cpu_init(void)
 {
-    /* trigger static peripheral initialization */
-    periph_init();
+  wdt_disable();
+  power_all_disable();
+  /*
+  power_spi_disable();
+  power_usart0_disable();
+  power_usart1_disable();
+  power_twi_disable();
+  power_adc_disable();
+  power_timer0_disable();
+  power_timer1_disable();
+  power_timer2_disable();
+  power_timer3_disable();
+  */
+
+  periph_init();
 }
