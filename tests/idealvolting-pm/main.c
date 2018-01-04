cd1f9a16de42f7078b4e101f0d1f98df8769f71d
Rasmus Antons <mail@rasmusantons.de>
2018-01-04 16:51:48 +0100

    idealvolting-pm: Test application

---
+++ tests/idealvolting-pm/main.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2017 Rasmus Antons <r.antons@tu-bs.de>
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @ingroup     tests
+ * @{
+ *
+ * @file
+ * @brief       Test application for the IdealVolting implementation in
+ *              boards_inga_common.
+ *
+ * @author      Rasmus Antons <r.antons@tu-bs.de>
+ *
+ * @}
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	while (1) {
+		puts("hello");
+	}
+	return 0;
+}
