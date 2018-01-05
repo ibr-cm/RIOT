da9575ca8d35ada321fca51bcc816d3e174d8ab6
Rasmus Antons <mail@rasmusantons.de>
2018-01-05 14:13:00 +0100

    idealvolting: Use rtt timer (does not work correctly)

--- tests/idealvolting-pm/main.c
+++ tests/idealvolting-pm/main.c
@@ -20,11 +20,30 @@
  */
 
 #include <stdio.h>
+#include <stdint.h>
+#include "idealvolting.h"
+#include "periph/uart.h"
+#include "uart_stdio.h"
+#include "periph/i2c.h"
+
+#define DELAY(x) for (unsigned long i = 0; i < 65535 >> x; ++i) \
+                	l = (double) i / (double) i + 0.00004
+
+extern uint8_t pm_blocker[4];
 
 int main(void)
 {
+	volatile double l = 0.0;
+	(void) l;
+	uart_poweroff(UART_STDIO_DEV);
 	while (1) {
-		puts("hello");
+		uint8_t b = pm_blocker[3];
+		//uart_poweron(UART_STDIO_DEV);
+		printf("%d\n", b);
+		idealvolting_print_status();
+		DELAY(4);
+		//uart_poweroff(UART_STDIO_DEV);
+		DELAY(2);
 	}
 	return 0;
 }
