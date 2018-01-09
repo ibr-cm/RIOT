a6e087e516bf0c7939b44ce8332a74c44df95242
Rasmus Antons <mail@rasmusantons.de>
2018-01-09 18:53:45 +0100

    idealvolting: prepare for use on different boards

--- boards/common/inga/idealvolting/include/idealvolting_config.h
+++ boards/common/inga/idealvolting/include/idealvolting_config.h
@@ -23,21 +23,35 @@
 #ifndef IDEALVOLTING_CONFIG_H
 #define IDEALVOLTING_CONFIG_H
 
+#if defined BOARD_INGA_BLUE
+#include "ad5242.h"
+#include "ad5242_params.h"
+#define vscale_t              ad5242_t
+#define VSCALE_INIT(x)        ad5242_init(x, &ad5242_params)
+#define VSCALE_SET_REG(x, y)  ad5242_set_reg(x, y)
+#elif defined BOARD_REAPER
+#include "max541x.h"
+#include "max541x_params.h"
+#define vscale_t              max541x_t
+#define VSCALE_INIT(x)        max541x_init(x, &max541x_params)
+#define VSCALE_SET_REG(x, y)  max541x_set_reg(x, y)
+#endif
+
 /**
  * @brief On the Tiny, synchronize the frame interval to the Megs's clock
  * on start
  */
-#define USE_MEGA_CLOCK 0
+#define USE_MEGA_CLOCK  0
 
 /**
  * @name I2C configuration
  * @{
  */
-#define IV_I2C_DEV        I2C_DEV(0)       /*< I2C device */
-#define SI_I2C_ADDR       0x2d             /*< Address of the Tiny */
-#define SI_I2C_SPEED      I2C_SPEED_NORMAL /*< I2C Speed */
-#define TMP_ADDR          0x48             /*< Address of the temp sensor */
-#define TMP_REG           0x00             /*< Register to read temp byte */
+#define IV_I2C_DEV    I2C_DEV(0)       /*< I2C device */
+#define SI_I2C_ADDR   0x2d             /*< Address of the Tiny */
+#define SI_I2C_SPEED  I2C_SPEED_NORMAL /*< I2C Speed */
+#define TMP_ADDR      0x48             /*< Address of the temp sensor */
+#define TMP_REG       0x00             /*< Register to read temp byte */
 /* @} */
 
 /**
@@ -45,20 +59,20 @@
  * @brief The OSCCAL will not be set outside these values
  * @{
  */
-#define IV_OSCCAL_MIN     0x9e
-#define IV_OSCCAL_MAX     0xbf
+#define IV_OSCCAL_MIN  0x9e
+#define IV_OSCCAL_MAX  0xbf
 /* @} */
 
 /**
  * @brief Initial voltage register value
  */
-#define IV_RESET_VREG     128
+#define IV_RESET_VREG  128
 
 /**
  * @brief MCU self check configuration
  * @{
  */
-#define MCU_CHECK()       alu_check((uint8_t) 1212987413.12)
+#define MCU_CHECK()               alu_check((uint8_t) 1212987413.12)
 #define MCU_CHECK_RESULT_TYPE     uint8_t
 #define MCU_CHECK_RESULT_CORRECT  19
 /* @} */
