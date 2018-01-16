6f94ba66ce119e2796b241edc8964542fc5b8a93
Rasmus Antons <mail@rasmusantons.de>
2018-01-16 14:30:14 +0100

    idealvolting-pm: use built-in delay function

--- tests/idealvolting-pm/main.c
+++ tests/idealvolting-pm/main.c
@@ -26,23 +26,18 @@
 #include "uart_stdio.h"
 #include "periph/i2c.h"
 
-#define DELAY(x) for (unsigned long i = 0; i < 65535 >> x; ++i) \
-                	l = (double) i / (double) i + 0.00004
-
 extern uint8_t pm_blocker[4];
 
 int main(void)
 {
-	volatile double l = 0.0;
-	(void) l;
-	//uart_poweroff(UART_STDIO_DEV);
+	uart_poweroff(UART_STDIO_DEV);
 	while (1) {
 		uint8_t b = pm_blocker[3];
-		//uart_poweron(UART_STDIO_DEV);
+		uart_poweron(UART_STDIO_DEV);
 		printf("%d\n", b);
-		DELAY(4);
-		//uart_poweroff(UART_STDIO_DEV);
-		DELAY(2);
+		__builtin_avr_delay_cycles(4000000);
+		uart_poweroff(UART_STDIO_DEV);
+		__builtin_avr_delay_cycles(8000000);
 	}
 	return 0;
 }
