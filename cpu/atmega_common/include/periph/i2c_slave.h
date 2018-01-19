f4c9640dfb89ab94cec5aa28ade9fe3e3ddb8a54
Rasmus Antons <mail@rasmusantons.de>
2018-01-19 16:21:34 +0100

    cpu/atmega_common: i2c slave calls a callback function to receive or transmit data

--- cpu/atmega_common/include/periph/i2c_slave.h
+++ cpu/atmega_common/include/periph/i2c_slave.h
@@ -1,11 +1,12 @@
 #ifndef PERIPH_I2C_SLAVE_H
 #define PERIPH_I2C_SLAVE_H
 
-void i2c_init_slave(uint8_t address);
-void i2c_stop_slave(void);
+#define I2C_SLAVE_MAX_FRAME_SIZE 8
 
-extern volatile uint8_t buffer_address;
-extern volatile uint8_t txbuffer[0xFF];
-extern volatile uint8_t rxbuffer[0xFF];
+typedef void(*i2c_slave_rcb_t)(uint8_t n_received, uint8_t *data_reveiced);
+typedef uint8_t(*i2c_slave_tcb_t)(uint8_t data_received, uint8_t *txbuffer);
+
+void i2c_init_slave(uint8_t address, i2c_slave_rcb_t rcb, i2c_slave_tcb_t tcb);
+void i2c_stop_slave(void);
 
 #endif /* PERIPH_I2C_SLAVE_H */
