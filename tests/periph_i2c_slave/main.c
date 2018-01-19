361c032a989b06b86b948a3eb1e4ac40bb56d216
Rasmus Antons <mail@rasmusantons.de>
2018-01-19 16:21:34 +0100

    cpu/atmega_common: i2c slave calls a callback function to receive or transmit data

--- tests/periph_i2c_slave/main.c
+++ tests/periph_i2c_slave/main.c
@@ -3,19 +3,30 @@
 
 #define MEGA_ADDR 0x2d
 
+void rcb(uint8_t n_received, uint8_t *data_received)
+{
+	printf("Data received: ");
+	for (uint8_t i = 0; i < n_received; ++i)
+		printf("%c ", data_received[i]);
+	puts("");
+}
+
+uint8_t tcb(uint8_t data_received, uint8_t *txbuffer)
+{
+	printf("Data requested: 0x%02x\n", data_received);
+	sprintf((char *) txbuffer, "ok");
+	return 3;
+}
+
 int main(void)
 {
 	puts("Test for the I2C-slave driver");
 
-	uint8_t c = 0;
-
-	i2c_init_slave(MEGA_ADDR);
+	i2c_init_slave(MEGA_ADDR, rcb, tcb);
 
 	while (1) {
-		if (rxbuffer[0] != c) {
-			c = rxbuffer[0];
-			printf("Data received: %c\n", c);
-		}
+		__builtin_avr_delay_cycles(32000000);
+		puts("ping");
 	}
 
 	return 0;
