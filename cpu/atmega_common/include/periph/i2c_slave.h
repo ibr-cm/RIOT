82aa107b086f61ea84667ab2b41f98985502e70b
Rasmus Antons <mail@rasmusantons.de>
2018-01-25 15:44:47 +0100

    cpu/atmega_common: i2c_slave doesn't expect register as first byte

--- cpu/atmega_common/include/periph/i2c_slave.h
+++ cpu/atmega_common/include/periph/i2c_slave.h
@@ -1,10 +1,12 @@
 #ifndef PERIPH_I2C_SLAVE_H
 #define PERIPH_I2C_SLAVE_H
 
+#include <stdint.h>
+
 #define I2C_SLAVE_MAX_FRAME_SIZE 8
 
 typedef void(*i2c_slave_rcb_t)(uint8_t n_received, uint8_t *data_reveiced);
-typedef uint8_t(*i2c_slave_tcb_t)(uint8_t data_received, uint8_t *txbuffer);
+typedef uint8_t(*i2c_slave_tcb_t)(uint8_t *txbuffer);
 
 void i2c_init_slave(uint8_t address, i2c_slave_rcb_t rcb, i2c_slave_tcb_t tcb);
 void i2c_stop_slave(void);
