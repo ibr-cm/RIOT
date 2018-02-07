cd5e5aab85deac178f32868d5213d8ae0434a1d0
Rasmus Antons <mail@rasmusantons.de>
2018-02-07 20:20:01 +0100

    tests/idealvolting-pm don't use the uart at all

--- tests/idealvolting-pm/main.c
+++ tests/idealvolting-pm/main.c
@@ -19,25 +19,16 @@
  * @}
  */
 
-#include <stdio.h>
 #include <stdint.h>
 #include "idealvolting.h"
-#include "periph/uart.h"
-#include "uart_stdio.h"
-#include "periph/i2c.h"
 
 extern uint8_t pm_blocker[4];
 
 int main(void)
 {
-	uart_poweroff(UART_STDIO_DEV);
 	while (1) {
-		uint8_t b = pm_blocker[3];
-		uart_poweron(UART_STDIO_DEV);
-		printf("%d\n", b);
-		__builtin_avr_delay_cycles(4000000);
-		uart_poweroff(UART_STDIO_DEV);
 		__builtin_avr_delay_cycles(8000000);
+		idealvolting_sleep(0);
 	}
 	return 0;
 }
