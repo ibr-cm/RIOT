ac3745e9858eb635e9c25ea63f1b94ec57aa0464
Martine Lenders <m.lenders@fu-berlin.de>
2018-08-24 16:11:30 +0200

    fixup! WIP: Adds crc_valid to netif (Resolved Merge Issues)

--- sys/net/gnrc/netif/gnrc_netif_ieee802154.c
+++ sys/net/gnrc/netif/gnrc_netif_ieee802154.c
@@ -154,6 +154,9 @@ static gnrc_pktsnip_t *_recv(gnrc_netif_t *netif)
 
             hdr->lqi = rx_info.lqi;
             hdr->rssi = rx_info.rssi;
+            if (rx_info.crc_valid) {
+                hdr->flags |= GNRC_NETIF_HDR_FLAGS_CRC_VALID;
+            }
             hdr->if_pid = thread_getpid();
             dev->driver->get(dev, NETOPT_PROTO, &pkt->type, sizeof(pkt->type));
 #if ENABLE_DEBUG
