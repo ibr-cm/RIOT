43e2aca2d10f24f8c0c0420c6043a78b6fee879a
Ding <czarsir@gmail.com>
2019-05-04 00:20:19 +0200

    [at86rf215] spi on one of the boards is not stable. make it lower. (maybe the oscillator not precise, or the wires are too long.)

--- drivers/at86rf215/include/at86rf215_params.h
+++ drivers/at86rf215/include/at86rf215_params.h
@@ -17,7 +17,7 @@ extern "C" {
 /*** board configuration ***/
 
 #define AT86RF215_PARAM_SPI         (SPI_DEV(0))
-#define AT86RF215_PARAM_SPI_CLK     (SPI_CLK_5MHZ)
+#define AT86RF215_PARAM_SPI_CLK     (SPI_CLK_1MHZ)
 #define AT86RF215_PARAM_CS          (GPIO_PIN(PORT_A, 4))
 #define AT86RF215_PARAM_INT         (GPIO_PIN(PORT_A, 15))
 #define AT86RF215_PARAM_SLEEP       (GPIO_PIN(PORT_B, 8))
