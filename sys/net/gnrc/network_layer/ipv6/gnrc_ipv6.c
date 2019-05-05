1e54e3fa99fd1edbdd026906d722aae54ca98628
Ding <czarsir@gmail.com>
2019-05-05 20:17:08 +0200

    [DEBUG] block NIB. need to recover later.

--- sys/net/gnrc/network_layer/ipv6/gnrc_ipv6.c
+++ sys/net/gnrc/network_layer/ipv6/gnrc_ipv6.c
@@ -217,7 +217,7 @@ static void *_event_loop(void *args)
             case GNRC_IPV6_NIB_DAD:
             case GNRC_IPV6_NIB_VALID_ADDR:
                 DEBUG("ipv6: NIB timer event received\n");
-                gnrc_ipv6_nib_handle_timer_event(msg.content.ptr, msg.type);
+                //gnrc_ipv6_nib_handle_timer_event(msg.content.ptr, msg.type);
                 break;
             default:
                 break;
