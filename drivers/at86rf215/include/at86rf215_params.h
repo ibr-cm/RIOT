a745bda97ef733caa4b6bf92e4c205be5bd29994
Ding <czarsir@gmail.com>
2019-05-09 18:11:38 +0200

    [at86rf215] use PE7 for IRQ.

--- drivers/at86rf215/include/at86rf215_params.h
+++ drivers/at86rf215/include/at86rf215_params.h
@@ -19,7 +19,7 @@ extern "C" {
 #define AT86RF215_PARAM_SPI         (SPI_DEV(0))
 #define AT86RF215_PARAM_SPI_CLK     (SPI_CLK_1MHZ)
 #define AT86RF215_PARAM_CS          (GPIO_PIN(PORT_A, 4))
-#define AT86RF215_PARAM_INT         (GPIO_PIN(PORT_A, 15))
+#define AT86RF215_PARAM_INT         (GPIO_PIN(PORT_E, 7))
 #define AT86RF215_PARAM_SLEEP       (GPIO_PIN(PORT_B, 8))
 #define AT86RF215_PARAM_RESET       (GPIO_PIN(PORT_B, 9))
 
