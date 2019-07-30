963a778ccc1cef865f666e7e38ef91a56ccdf32b
Torben Petersen <petersen@ibr.cs.tu-bs.de>
2019-07-30 14:17:12 +0200

    Adds attiny driver for pcf85063 rtc

---
+++ boards/common/inga/idealvolting/tiny/drv/pcf85063.c
@@ -0,0 +1,65 @@
+#include "pcf85063.h"
+#include "usi_i2c_master.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <util/delay.h>
+
+usi_twi_result_t pcf85063_init(void) {
+    usi_twi_result_t res;
+    /// Software Reset
+    uint8_t data[] = {PCF85063_REG_CONTROL_1, 0x58};
+    res = i2c_write_bytes(PCF85063_ADDR, data, sizeof(data));
+
+    /// STOP
+    data[1] = 0b00100000;
+    res = i2c_write_bytes(PCF85063_ADDR, data, sizeof(data));
+
+    _delay_ms(10);
+
+    data[1] = 0b00000000;
+    res = i2c_write_bytes(PCF85063_ADDR, data, sizeof(data));
+
+    return res;
+}
+
+usi_twi_result_t pcf85063_reset_flags(void) {
+    usi_twi_result_t res;
+    uint8_t data[2] = {0,0};
+    /// Enable alarm interrupt, clear AF and TF
+    data[0] = PCF85063_REG_CONTROL_2;
+    data[1] = 0b10000000;
+    res = i2c_write_bytes(PCF85063_ADDR, data, 2);
+    return res;
+}
+
+/**
+ * Set up countdown mode
+ */
+
+usi_twi_result_t pcf85063_set_countdown(uint8_t val) {
+    usi_twi_result_t res;
+    uint8_t data[2] = {0,0};
+
+    /// Set Mode
+    data[0] = PCF85063_REG_TIMER_MODE;
+    data[1] = 0b00010111;
+    res = i2c_write_bytes(PCF85063_ADDR, data, 2);
+    if(res != USI_TWI_SUCCESS) {
+        return res;
+    }
+
+    /// Set Timer value register
+    data[0] = PCF85063_REG_TIMER_VALUE;
+    data[1] = val;
+
+    res = i2c_write_bytes(PCF85063_ADDR, data, 2);
+    if(res != USI_TWI_SUCCESS) {
+        return res;
+    }
+
+    /// ...
+
+    pcf85063_reset_flags();
+
+    return res;
+}
---
+++ boards/common/inga/idealvolting/tiny/drv/pcf85063.h
@@ -0,0 +1,41 @@
+#ifndef __PCF85063_H__
+#define __PCF85063_H__
+
+#include <stdint.h>
+#include "usi_i2c_master.h"
+
+#define PCF85063_ADDR                   (0b1010001)
+
+#define PCF85063_REG_CONTROL_1          (0x00)
+#define PCF85063_REG_CONTROL_2          (0x01)
+#define PCF85063_REG_OFFSET             (0x02)
+#define PCF85063_REG_RAM_BYTE           (0x03)
+#define PCF85063_REG_SECOND             (0x04)
+#define PCF85063_REG_MINUTE             (0x05)
+#define PCF85063_REG_HOURS              (0x06)
+#define PCF85063_REG_DAYS               (0x07)
+#define PCF85063_REG_WEEKDAYS           (0x08)
+#define PCF85063_REG_MONTHS             (0x09)
+#define PCF85063_REG_YEARS              (0x0A)
+#define PCF85063_REG_SECOND_ALARM       (0x0B)
+#define PCF85063_REG_MINUTE_ALARM       (0x0C)
+#define PCF85063_REG_HOUR_ALARM         (0x0D)
+#define PCF85063_REG_DAY_ALARM          (0x0E)
+#define PCF85063_REG_WEEKDAY_ALARM      (0x0F)
+
+#define PCF85063_REG_TIMER_VALUE        (0x10)
+#define PCF85063_REG_TIMER_MODE         (0x11)
+
+typedef struct __attribute__((packed)) {
+    unsigned int unused : 3;
+    unsigned int tcf : 2;
+    unsigned int te : 1;
+    unsigned int tie : 1;
+    unsigned int ti_tp : 1;
+} pcf85063_reg_timer_mode_t;
+
+usi_twi_result_t pcf85063_init(void);
+usi_twi_result_t pcf85063_set_countdown(uint8_t val);
+usi_twi_result_t pcf85063_reset_flags(void);
+
+#endif
