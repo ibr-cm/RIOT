00387f30901e46186d1dc412f7847f8de4bfb28d
Angelo Steinmetzer <a.steinmetzer@tu-bs.de>
2020-11-21 16:36:41 +0100

    add Kconfig to ingas

--- makefiles/tools/targets.inc.mk
+++ makefiles/tools/targets.inc.mk
@@ -48,4 +48,4 @@ $(RIOTTOOLS)/flatc/flatc: $(RIOTTOOLS)/flatc/Makefile
 $(RIOTTOOLS)/inga_tool/inga_tool: $(RIOTTOOLS)/inga_tool/Makefile
 	@echo "[INFO] inga_tool binary not found - building it from source now"
 	CC= CFLAGS= $(MAKE) -C $(RIOTTOOLS)/inga_tool
-	@echo "[INFO] edbg binary successfully built!"
+	@echo "[INFO] inga_tool binary successfully built!"
