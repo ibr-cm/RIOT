364ff6009874e1dc31a1285e5c699dc1f48d43b5
Torben Petersen <petersen@ibr.cs.tu-bs.de>
2021-07-22 12:15:04 +0200

    drivers/at86rf215: implements crc valid flag

--- drivers/at86rf215/at86rf215_netdev.c
+++ drivers/at86rf215/at86rf215_netdev.c
@@ -198,6 +198,8 @@ static int _recv(netdev_t *netdev, void *buf, size_t len, void *info)
 
     if (info != NULL) {
         netdev_ieee802154_rx_info_t *radio_info = info;
+        /* radio drops invalid flags, so all received flags are crc valid by default! */
+        radio_info->crc_valid = 1;
         radio_info->rssi = (int8_t) at86rf215_reg_read(dev, dev->RF->RG_EDV);
     }
 
